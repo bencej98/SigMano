977a408e9726cfa34319a1e376151d93ea38592f
Andras <toth.andras.2000@gmail.com>
2023-06-08 13:47:52 +0200

    accept server to client messages

--- Client/client_socket/client_socket.py
+++ Client/client_socket/client_socket.py
@@ -4,9 +4,13 @@ import threading
 
 
 class ClientConnection:
+
     def __init__(self, host="localhost", port=10000) -> None:
         self.init_message = "teszt hello"
         self.action_types = ["Action","Registration","Closed"]
+
+        self.incomming = Incomming()
+        self.outgoing = Outgoing()
 
         self.connect_to_server(host, port)
 
@@ -14,9 +18,9 @@ class ClientConnection:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             client_socket.connect((HOST, PORT))
 
-            self.send_message(client_socket, self.init_message)
+            #self.send_message(client_socket, self.init_message)
 
-            incomming_messages=threading.Thread(target=self.accept_incoming, args=(client_socket,))
+            incomming_messages=threading.Thread(target=self.incomming.accept_incoming, args=(client_socket,))
             incomming_messages.start()
 
             while True:
@@ -41,19 +45,25 @@ class ClientConnection:
                 match action:
                     case "Action":
                         test_action = {"1": "hit","2": "defend"}
-                        self.send_message(client_socket, self.action_message(test_action))
+                        self.send_message(client_socket, self.outgoing.action_message(test_action))
                         
                     case "Registration":
                         test_user_name =  {"username": "xy","password": "xy"}
-                        self.send_message(client_socket, self.registration_message(test_user_name))
+                        self.send_message(client_socket, self.outgoing.registration_message(test_user_name))
                         
                     case "Closed":
-                        self.send_message(client_socket, self.close_message())
+                        self.send_message(client_socket,self.outgoing.close_message())
 
     def send_message(self,client_socket, message):
         client_socket.sendall(json.dumps(message).encode("utf-8"))
 
 #region OUTGOING MESSAGES:
+
+class Outgoing():
+
+    def __init__(self) -> None:
+        pass
+
     def action_message(self, action: dict) -> dict:
         # example: {"Type": "Action",Payload": {"1": "hit","2": "defend"}}
         return {"Type": "Action", "Payload": action}
@@ -66,18 +76,31 @@ class ClientConnection:
         # example: {"Type": "Registration",Payload": {"username": "xy","password": "xy"}}
         return {"Type": "Closed", "Payload":{}}    
 #endregion
+
 #region INCOMING MESSAGES:
+class Message():
+    def __init__(self, incoming_type, payload) -> None:
+        self.Type = incoming_type
+        self.Payload = payload
+
+class Incomming():
+    def __init__(self) -> None:
+        self.positions = None
+        self.event = None
+        pass
+
     def accept_incoming(self, client_socket):
         try:
             while True:
                 data = client_socket.recv(2048)
-
                 if not data:
                     print("Server disconnected!")
                     break
 
                 incoming = json.loads(data.decode("utf-8"))
-                print(f"FROM SERVER:{incoming}")
+                new_message = Message(incoming["Type"], incoming["Payload"])
+
+                print(f"FROM SERVER: {new_message.__dict__}")
 
         except Exception as e:
             print("EXCEPTION", e)
@@ -85,4 +108,7 @@ class ClientConnection:
 
 #endregion
 
+# ClientConnection("172.4.181.76", 8000)
+#ClientConnection("172.4.181.232", 10000)
 ClientConnection()
+
