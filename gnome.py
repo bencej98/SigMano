5bdeb32b6e9a27194a4649dcb95c2bc13d756637
Vasvári Gergő <gergo.vasvari@gmail.com>
2023-06-08 11:23:02 +0200

    added gnome movement

--- gnome.py
+++ gnome.py
@@ -13,7 +13,47 @@ class Gnome:
     def spawn_gnome(self, map):
         self.location["x"] = random.randint(0, map.x_coordinate)
         self.location["y"] = random.randint(0, map.y_coordinate)
+
+    def random_move(self):
+        temp_coord=self.location
+
+        direction=random.randint(0,7)
+        coordinate_is_valid = False
+        while coordinate_is_valid == False:
+            match direction:
+                # 0 is up then clockwise
+                case 0: 
+                    temp_coord["y"] += 1 
+                case 1: 
+                    temp_coord["x"] += 1
+                    temp_coord["y"] += 1 
+                case 2: 
+                    temp_coord["x"] +=1 
+                case 3: 
+                    temp_coord["x"] += 1
+                    temp_coord["y"] -= 1  
+                case 4: 
+                    temp_coord["y"] -= 1 
+                case 5: 
+                    temp_coord["x"] -= 1
+                    temp_coord["y"] -= 1 
+                case 6: 
+                    temp_coord["x"] -= 1 
+                case 7: 
+                    temp_coord["x"] -= 1
+                    temp_coord["y"] += 1         
+            coordinate_is_valid = self.check_if_location_is_valid(temp_coord)
         
+        self.location = temp_coord
+
+            
+
+
+    def check_if_location_is_valid(map, coord):
+        if map.x_coordinate >= coord["x"] >= 0 and map.y_coordinate >= coord["y"] >= 0:
+            return True
+        return False
+
 
 class Map:
     def __init__(self) -> None:
