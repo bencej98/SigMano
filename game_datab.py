0becc351fa2dc430b3977412ecbaf9bf99e8cb76
Gabor Slezak <gaborslezak@protonmail.com>
2023-06-09 10:06:03 +0200

    Added update_sumscore_upon_death and formatted the code according to PEP8.
    Co-Authored-By: Bence Jenei <101675772+bencej98@users.noreply.github.com>

--- game_datab.py
+++ game_datab.py
@@ -2,6 +2,7 @@ import sqlite3
 import os.path
 from datetime import datetime
 
+
 class Gnome_Database:
     path = "game_database.db"
 
@@ -11,18 +12,20 @@ class Gnome_Database:
         self.create_table()
 
     def create_table(self):
-        '''Creates the database'''
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS "user" (
+        """Creates the database"""
+        self.cursor.execute(
+            """CREATE TABLE IF NOT EXISTS "user" (
                 "id" INTEGER NOT NULL,
                 "username" TEXT UNIQUE,
                 "password" TEXT,
                 "gnome" TEXT UNIQUE,
                 "sumscore" INTEGER,
                 PRIMARY KEY("id" AUTOINCREMENT)
-            )''')
-        
+            )"""
+        )
+
         self.cursor.execute(
-            '''
+            """
             CREATE TABLE IF NOT EXISTS "match" (
             "match_id"	INTEGER NOT NULL,
             "username"	TEXT,
@@ -30,24 +33,26 @@ class Gnome_Database:
             "score"	INTEGER,
             "date" TEXT NOT NULL,
             PRIMARY KEY("match_id" AUTOINCREMENT)
-            )'''
+            )"""
         )
         self.connect.commit()
-        
+
     def close_connection(self):
-        '''Closes the database connection'''
+        """Closes the database connection"""
         self.cursor.close()
         self.connect.close()
 
     def create_user(self, username, password, gnome_name):
-        '''Inserts user into database'''
-        self.cursor.execute('''
+        """Inserts user into database"""
+        self.cursor.execute(
+            """
                         INSERT INTO user
                         VALUES(?, ?, ?, ?, ?)
-                        '''
-                         , (None, username, password, gnome_name, None))
+                        """,
+            (None, username, password, gnome_name, None),
+        )
         self.connect.commit()
-    
+
     def delete_user(self, username):
         self.cursor.execute("DELETE FROM user WHERE username = ?", (username,))
         row_count = self.cursor.rowcount
@@ -60,22 +65,40 @@ class Gnome_Database:
         self.connect.commit()
 
     def print_sum_point(self, username):
-        self.cursor.execute('''
+        self.cursor.execute(
+            """
                         SELECT sumscore FROM user
                         WHERE username = ?
-                        '''
-                         , (username,))
+                        """,
+            (username,),
+        )
         print(f"TEST user's score: {self.cursor.fetchone()[0]}")
 
     def add_results_upon_death(self, username, kill_count, score):
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.cursor.execute('''
+        self.cursor.execute(
+            """
                             INSERT INTO match
                             VALUES (?, ?, ?, ?, ?)
-        '''
-        , (None, username, kill_count, score, current_time))
+        """,
+            (None, username, kill_count, score, current_time),
+        )
+
         self.connect.commit()
 
+        self.update_sumscore_upon_death(username, score)
+
+    def update_sumscore_upon_death(self, username, score):
+        self.cursor.execute(
+            """
+                    UPDATE user
+                    SET sumscore = sumscore + ?
+                    WHERE username = ?
+                    """,
+            (score, username),
+        )
+
+        self.connect.commit()
 
 
 jatek = Gnome_Database()
