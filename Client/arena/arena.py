c40ec030e465749620b527dfa55c6ebe47c213d7
McLoad6 <mcload6@yahoo.com>
2023-06-09 10:38:53 +0200

    Get moves and obj builds from dict

--- Client/arena/arena.py
+++ Client/arena/arena.py
@@ -20,8 +20,8 @@ class Player(turtle.Turtle):
       self.name1.write(name, False, "center")
 
    def move(self, x:int, y:int):
-      x = tile_size*(x-9.5)
-      y = tile_size*(y-9.5)
+      x = x_y_for_screen(x)
+      y = x_y_for_screen(y)
       self.name1.goto(x, y+self.size*10)
       self.name1.clear()
       self.goto(x, y)
@@ -30,6 +30,9 @@ class Player(turtle.Turtle):
 tile_size = 30
 tile_number = 20
 
+def x_y_for_screen(coordinate:int):
+   return tile_size*(coordinate-9.5)
+
 # TODO: Flexible window later
 
 turtle.setup(tile_size*tile_number, tile_size*tile_number, None, None)
@@ -37,21 +40,39 @@ window = turtle.Screen()
 window.title("Arena")
 window.bgcolor("lightgreen")
 
-Player1 = Player("Marine", -tile_size*9.5 , tile_size*0.5, "red", tile_size/60)
-Player2 = Player("Zerling", -tile_size*9.5 , tile_size*1.5, "black", tile_size/60)
-Player3 = Player("Zealot", -tile_size*9.5 , tile_size*2.5, "blue", tile_size/60)
+def create_object(object_name:str, coordinates:list):
+   x = x_y_for_screen(coordinates[0])
+   y = x_y_for_screen(coordinates[1])
+   obj = Player(object_name, x, y, "black", tile_size/60)
+   return obj
 
+def get_obj_from_list(name:str,object_list:list):
+   for i in object_list:
+      if i.name == name:
+         return i
 
-for i in range(100):
+def dict_data_for_screen(jason_dict:dict, object_list:list):
+   obj_names = []
+   for i in object_list:
+      obj_names.append(i.name)
+   for name, coordinates in jason_dict.items():
+      if name in obj_names:
+         obj = get_obj_from_list(name, object_list)
+         obj.move(coordinates[0], coordinates[1])
+      else:
+         obj = create_object(name,coordinates)
+         object_list.append(obj)
+
+object_list = []
+
+for i in range(10):
    x = random.randint(0,19)
    y = random.randint(0,19)
-   Player1.move(x, y)
-   x = random.randint(0,19)
-   y = random.randint(0,19)
-   Player2.move(x, y)
-   x = random.randint(0,19)
-   y = random.randint(0,19)
-   Player3.move(x,y)
-   #time.sleep(1)
+   name = random.randint(1,3)
+   x2 = random.randint(0,19)
+   y2 = random.randint(0,19)
+   name2 = random.randint(1,3)
+   jason = {name:[x,y], name2:[x2,y2]}
+   dict_data_for_screen(jason, object_list)
 
 window.mainloop()
