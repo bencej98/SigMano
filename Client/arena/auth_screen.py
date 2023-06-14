97147f29f6a084bb2e0561c20e111dc09be7c1e4
Robin Gerg <gergrobin@gmail.com>
2023-06-14 12:26:28 +0200

    Warn user using important passwords here

--- Client/arena/auth_screen.py
+++ Client/arena/auth_screen.py
@@ -119,10 +119,12 @@ class RegisterPage(tk.Frame):
         password_label_1 = tk.Label(self, text="Password: ", font=controller.label_font, background="skyblue")
         password_1 = tk.StringVar()
         self.password_entry_1 = tk.Entry(self, textvariable=password_1, font=controller.label_font, show="*")
+        self.password_entry_1.bind("<1>", self.warn_about_password)
         # password 2
         password_label_2 = tk.Label(self, text="Password again: ", font=controller.label_font, background="skyblue")
         password_2 = tk.StringVar()
         self.password_entry_2 = tk.Entry(self, textvariable=password_2,font=controller.label_font, show="*")
+        self.password_entry_2.bind("<1>", self.warn_about_password)
 
         register_button = tk.Button(self, text="Register",
                            command=lambda: self._register_user(username, password_1, password_2), font=self.controller.button_font, width=10, background="RoyalBlue", fg="white")
@@ -188,6 +190,9 @@ class RegisterPage(tk.Frame):
 
         hashed_pw = password.get()
         return hashed_pw
+    
+    def warn_about_password(self, *arg):
+        messagebox.showinfo("Warning", "Don't use important password here!")
     
     def _empty_entry_fields(self):
         """Empties all entry fields"""
