873a74a92ad6982b9eb56eb2d1bc9883257408c2
Vasvári Gergő <gergo.vasvari@gmail.com>
2023-06-18 23:37:18 +0200

    added moving towrds fight

--- action_manager.py
+++ action_manager.py
@@ -1,10 +1,13 @@
-from gnome import Map
+from gnome import Map, Gnome
 
 class ActionManager:
     def __init__(self) -> None:
-        self.collided_gnomes = []
+        self.collided_gnomes = {}
         self.event_dictionary = {}
         self.user_strategies = {}
+        #TODO
+        self.was_fight = False
+        self.was_death = False
         
     def _get_collided_gnomes(self, map: Map):
         self.collided_gnomes = map.check_collisions()
@@ -21,12 +24,13 @@ class ActionManager:
                         updated_gnomes.append(user)
         for user in updated_gnomes:
             del self.user_strategies[user]
-        
 
     def fight(self, map):
+        self.was_fight = False
         self.event_dictionary = {}
         self._get_collided_gnomes(map)
         if len(self.collided_gnomes) > 0:
+            self.was_fight = True
             for gnomes in self.collided_gnomes:
                 for i, gnome in enumerate(gnomes):
                     if i < len(gnomes) - 1:
@@ -47,23 +51,46 @@ class ActionManager:
         return self.event_dictionary
     
     def check_gnome_death(self, map: Map):
+        self.was_death = False
         gnome_deathnote = []
         for gnome_name, gnome in map.active_gnomes.items():
             if gnome.lose_count >= 3:
                 gnome_deathnote.append(gnome_name)
         
-        for gnome_name in gnome_deathnote:
-            dead_gnome = map.active_gnomes.pop(gnome_name)
-            dead_gnome.event_counter = 0
-            dead_gnome.actual_points = 0
-            dead_gnome.kill_count = 0
-            dead_gnome.lose_count = 0
-            map.add_gnome_to_gnome_queue(dead_gnome)
+        if len(gnome_deathnote) > 0:
+            self.was_death = True
+            for gnome_name in gnome_deathnote:
+                dead_gnome = map.active_gnomes.pop(gnome_name)
+                dead_gnome.event_counter = 0
+                dead_gnome.actual_points = 0
+                dead_gnome.kill_count = 0
+                dead_gnome.lose_count = 0
+                map.add_gnome_to_gnome_queue(dead_gnome)
         
         return {
             "Type": "Death",
             "Payload": gnome_deathnote
                 }
+    
+    def move_all_gnomes(self, map: Map):
+        map.update_gnomes_distances()
+        position_update_dict = {}
+        for gnome_name, gnome in self.active_gnomes.items():
+            gnome.random_move(map)
+            position = (gnome.location["x"], gnome.location["y"])
+            position_update_dict[gnome.user] = position
+        position_update_for_client = {"Type": "Position", "Payload": position_update_dict}
+        return position_update_for_client
+
+    def _move_towards_fight(self, gnome: Gnome, map: Map):
+        closest_fight_distance = map.x_coordinate
+        closest_fight_direction = 0
+        for fight_location in self.collided_gnomes:
+            figth_vector = map.calculate_distance(gnome.location, fight_location)
+            if figth_vector["distance"] < closest_fight_distance:
+                closest_fight_distance = figth_vector["distance"]
+                closest_fight_direction = figth_vector["direction"]
+        gnome.move_towards_direction(closest_fight_direction, map)
 
     def _check_fight_option(self, gnome_first, gnome_second):
         gnome_first_action = gnome_first.strategy[gnome_first.event_counter]
