45c905516ac178b835144aa6dadea3933e3d9b2f
Vasvári Gergő <gergo.vasvari@gmail.com>
2023-06-19 13:47:32 +0200

    added new aciton manager

--- action_manager.py
+++ action_manager.py
@@ -1,10 +1,13 @@
-from gnome import Map
+from gnome import Map, Gnome
 
 class ActionManager:
     def __init__(self) -> None:
-        self.collided_gnomes = []
+        self.collided_gnomes = {}
         self.event_dictionary = {}
         self.user_strategies = {}
+        #TODO
+        self.was_fight = False
+        self.was_death = False
         
     def _get_collided_gnomes(self, map: Map):
         self.collided_gnomes = map.check_collisions()
@@ -21,13 +24,14 @@ class ActionManager:
                         updated_gnomes.append(user)
         for user in updated_gnomes:
             del self.user_strategies[user]
-        
 
     def fight(self, map):
+        self.was_fight = False
         self.event_dictionary = {}
         self._get_collided_gnomes(map)
         if len(self.collided_gnomes) > 0:
-            for gnomes in self.collided_gnomes:
+            self.was_fight = True
+            for gnomes in self.collided_gnomes.values():
                 for i, gnome in enumerate(gnomes):
                     if i < len(gnomes) - 1:
                         for j in range(i+1, len(gnomes)):
@@ -47,6 +51,7 @@ class ActionManager:
         return self.event_dictionary
     
     def check_gnome_death(self, map: Map):
+        self.was_death = False
         gnome_deathnote = []
         for gnome_name, gnome in map.active_gnomes.items():
             if gnome.lose_count >= 3:
@@ -73,6 +78,53 @@ class ActionManager:
             "Type": "Death",
             "Payload": death_payload
                 }
+    
+    def move_all_gnomes(self, map: Map):
+        map.update_gnomes_distances()
+        position_update_dict = {}
+        for gnome_name, gnome in map.active_gnomes.items():
+            if gnome.direction == None:
+                gnome.random_move(map)
+            else:
+                gnome.move_towards_direction(map)
+            position = (gnome.location["x"], gnome.location["y"])
+            position_update_dict[gnome.user] = position
+        position_update_for_client = {"Type": "Position", "Payload": position_update_dict}
+        return position_update_for_client
+
+    def _set_target_towards_fight(self, gnome: Gnome, map: Map):
+        if gnome.has_reached_target():
+            closest_fight_distance = map.x_coordinate
+            closest_fight_location = {}
+            for fight_location in self.collided_gnomes:
+                position_dict = {"x": fight_location[0], "y":fight_location[1]}
+                figth_vector = map.calculate_distance(gnome.location, position_dict)
+                if figth_vector["distance"] < closest_fight_distance:
+                    closest_fight_distance = figth_vector["distance"]
+                    closest_fight_location["x"] = fight_location[0]
+                    closest_fight_location["y"] = fight_location[1]
+
+            gnome.target_location = closest_fight_location
+            gnome.update_direction(map)
+
+    def check_action(self, gnome: Gnome, strategy):
+        if strategy["action"] == "runaway":
+            gnome.turn_against_direction()
+        elif strategy["action"] == "approach":
+            pass
+
+    def choose_strategy(self, map: Map):
+        for gnome_name, gnome in map.active_gnomes:
+            if len(gnome.target_location) == 0:
+                for strategy in gnome.event_reactions:
+                    if strategy["event"] == "fight" and self.was_fight:
+                        self._set_target_towards_fight(gnome)
+                        self.check_action(gnome, strategy)
+                    if strategy["event"] == "":
+                        pass
+                    else:
+                        gnome.direction = None
+
 
     def _check_fight_option(self, gnome_first, gnome_second):
         gnome_first_action = gnome_first.strategy[gnome_first.event_counter]
