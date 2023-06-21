d4087f123ba5117354afc8721af0dec65fbb2fa8
Valami-Laci <l.pohlhu@gmail.com>
2023-06-21 15:13:24 +0200

    Update server_init.py
    fixed indentation

--- Server/server_init.py
+++ Server/server_init.py
@@ -4,14 +4,14 @@ from gnome import *
 import time
 
 def main():
-        travel = Map(4, 4, 5)
-        action = ActionManager()
-        server = Gameserver(travel, action)
-        server.db.create_table()
-        server.run_tik_data_thread()
-        while True:            
-            server.process_data()
-            time.sleep(2)
+    travel = Map(4, 4, 5)
+    action = ActionManager()
+    server = Gameserver(travel, action)
+    server.db.create_table()
+    server.run_tik_data_thread()
+    while True:            
+        server.process_data()
+        time.sleep(2)
 
 if __name__ == "__main__":
     try:
